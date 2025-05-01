801aa47237e6a01982349ef8e1587271cbaaf902
Owen <osowatzke@gmail.com>
2025-04-30 19:45:04 -0700

    Cleaning up routine which solved for MFP

--- code/XM_constants.py
+++ code/XM_constants.py
@@ -1,19 +1,23 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 class XM_sat:
-    def __init__(self, fs=1.64e6, oversample=1):
+    def __init__(self, fs=1.64e6):
+
+        # Save input parameters
         self.fs=fs
-        self.symbol_rate=1.64e6
-        self.frame_time=.432
-        self.MFP_symbols=64
-        self.FSP_symbols=32
-        self.Data_symbols=3424
-        self.Data_symbols_last=3264 #note, last FSP has shorter Data_symbols + PAD
-        self.PAD_symbols=96
-        self.FSP_per_frame=205
-        self.samples_per_frame=int(self.MFP_symbols+self.FSP_per_frame*(self.FSP_symbols+self.Data_symbols)+\
-            self.PAD_symbols-(self.Data_symbols-self.Data_symbols_last))
-        #assuming starts at 1
+
+        # Parameters from US8260192
+        self.symbol_rate    = 1.64e6
+        self.frame_time     = 0.432
+        self.MFP_symbols    = 64
+        self.FSP_symbols    = 32
+        self.data_symbols   = 3424
+        self.pad_symbols    = 96
+        self.FSP_per_frame  = 205
+        
+        # Preambles computed from the data. Ambiguous by 180 degrees.
+        # Assuming first BPSK symbol is a 1.
         FSP = -np.array([-1,  1, -1,  1, -1, -1,  1,  1, -1,  1,  1,  1, -1, -1, -1, -1,
                         1, -1,  1, -1, -1,  1,  1, -1,  1,  1,  1, -1, -1, -1, -1,  1])
         self.FSP=FSP*(1+1j)
@@ -22,34 +26,88 @@ class XM_sat:
                          1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, 1, -1,
                          -1, -1, 1, -1, 1, 1])
         self.MFP=MFP*(1+1j)
-        self.samples_between_frames=self.samples_per_frame*fs/self.symbol_rate
-        self.samples_between_FSP=int((self.Data_symbols+self.FSP_symbols)*fs/self.symbol_rate)
 
     def get_freq_est(self, data, debug=False):
+
+        # Determine the span of the FFT
         span=int(self.samples_between_FSP)
+        
+        # Locate the FSP
         corr_out=np.correlate(data, self.FSP)
         max_FSP=np.argmax(np.abs(corr_out))
-        print("FIRST FSP found at ", max_FSP)
-        # take 4th power to find freq
-        temp_data_sq=data*data
-        freq_fft=np.abs(np.fft.fft(temp_data_sq*temp_data_sq))
+        print("FSP found at ", max_FSP)
+
+        # Raise data to the 4th power to remove QPSK modulation
+        freq_fft=np.abs(np.fft.fft(data**4))
         max_freq_bin=np.argmax(freq_fft)
         print("freq bin = ", max_freq_bin)
-        freq_per_bin = self.fs/(4*span) #power of 4 with span
+
+        # Scale frequency by 1/4 to remove effects of raising data to the 4th power
+        freq_per_bin = self.fs/(4*span)
+
+        # Estimate fractional bin offset
         bin_offset=self.interp(freq_fft[max_freq_bin-1], freq_fft[max_freq_bin], freq_fft[max_freq_bin+1])
+
+        # Determine frequency offset in Hz
         course_freq_est=(max_freq_bin+bin_offset)*freq_per_bin
         print ("Course freq estimate = ", course_freq_est)
+
+        # Plot the angle of the data after removing the frequency offset
+        # Should not drift across time
         if (debug):
             t=np.arange(span)/self.fs
             temp_data=data*np.exp(-1j*2*np.pi*course_freq_est*t)
             plt.plot(np.angle(temp_data),'b.')
             plt.show()
-        return(course_freq_est)
+        return course_freq_est
+    
     def interp(self, ym1,y0,yp1):
         p = (yp1 - ym1)/(2*(2*y0 - yp1 - ym1)); #position
         y = y0 - 0.25*(ym1-yp1)*p; #height
-        return(p)
+        return p
 
+    # Define dependent read-only properties with getter function
+    @property
+    def symbols_per_frame(self):
+        # Last frame includes implict padding and MFP
+        return int(self.FSP_per_frame*(self.FSP_symbols+self.data_symbols))
+    
+    @property
+    def symbol_oversample_rate(self):
+        return self.fs/self.symbol_rate
+    
+    @property
+    def samples_per_MFP(self):
+        return int(self.MFP_symbols*self.symbol_oversample_rate)
+
+    @property
+    def samples_between_MFP(self):
+        return int(self.frame_time*self.fs)
+
+    @property
+    def samples_per_FSP(self):
+        return int(self.FSP_symbols*self.symbol_oversample_rate)
+    
+    @property
+    def samples_between_FSP(self):
+        return int((self.FSP_symbols+self.data_symbols)*self.symbol_oversample_rate)
+    
+    @property
+    def samples_per_frame(self):
+        return int(self.symbols_per_frame*self.symbol_oversample_rate)
+
+    @property
+    def samples_per_second(self):
+        return self.fs
+    
+    # Print configuration for XM satellite object
+    def print_config(self):
+        print("samples per MFP = ",     self.samples_per_MFP)
+        print("samples between MFP = ", self.samples_between_MFP)
+        print("samples per FSP = ",     self.samples_per_FSP)
+        print("samples between FSP = ", self.samples_between_FSP)
+        print("samples per frame = ",   self.samples_per_frame)
+        print("samples per second = ",  self.samples_per_second)
 
     class XM_interleaver:
         def __init__(self, buffer_size, step_size):
--- code/XM_find_MFP.py
+++ code/XM_find_MFP.py
@@ -4,65 +4,80 @@ import sys
 import signal
 sys.path.append("/media/sf_VM_shared/ECE531/Final/python_tools")
 import XM_constants as XM
+
+# Allow user to exit out of script early
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# Create debug plots when true
+debug = True
 
-filename = "XM_test_x2_sync.dat"
-data_type = np.complex64  # Replace with the correct data type
+# Create XM object
 XM=XM.XM_sat()
-print("samples between MFP = ", int(XM.frame_time*XM.fs))
-print("samples per MFP = ", (XM.MFP_symbols*XM.fs/XM.symbol_rate))
-print("samples per FSP = ", (XM.FSP_symbols*XM.fs/XM.symbol_rate))
-print("samples between FSP = ", XM.Data_symbols*XM.fs/XM.symbol_rate)
-print("samples per frame = ",XM.samples_per_frame*XM.fs/XM.symbol_rate )
-samples_between_frames=int(XM.samples_per_frame*XM.fs/XM.symbol_rate)
-samples_between_FSP=int(XM.FSP_symbols+XM.Data_symbols*XM.fs/XM.symbol_rate)
-print("samples per second = ",XM.samples_per_frame*XM.fs/(XM.symbol_rate*XM.frame_time) )
+XM.print_config()
 
+# Load samples from file
+filename = "XM_test_x2_sync.dat"
+data_type = np.complex64
 with open(filename, "rb") as file:
     data_array = np.fromfile(file, dtype=data_type)
-samples=len(data_array)
-print(samples/XM.fs)
-# about 2 seconds in, then locked
-idx=int(2*XM.fs);
-FSP_in_file=int(samples/samples_between_FSP)
-MFP_in_file=int(samples/samples_between_frames)
+samples = len(data_array)
+
+# Display the amount of information in the frame
+print("Data Duration (s) = ", samples/XM.fs)
+
+FSP_in_file=int(samples/XM.samples_between_FSP)
+MFP_in_file=int(samples/XM.samples_between_MFP)
 
 print ("FSP in file = ", FSP_in_file)
 print ("MFP in file = ", MFP_in_file)
 
-# find first FSP
-corr_out=np.correlate(data_array[idx:idx+samples_between_FSP+XM.FSP_symbols],XM.FSP,  'full')
+# Find first FSP
+# Data takes roughly 2s to lock (so ignore first 2s of data)
+idx=int(2*XM.fs);
+corr_out=np.correlate(data_array[idx:idx+XM.samples_between_FSP+XM.FSP_symbols], XM.FSP, 'full')
 fsp_idx=idx+np.argmax(np.abs(corr_out))-XM.FSP_symbols+1
 print ("first fsp found at ", fsp_idx, idx)
 print ("FSP CHECK, should be the same")
 print (1*(data_array[fsp_idx:fsp_idx+XM.FSP_symbols]>0))
 print (1*(XM.FSP>0))
 
-idx=fsp_idx-XM.MFP_symbols #MFP prior to FSP
+# Find MFP prior to FSP
+idx = fsp_idx-XM.MFP_symbols
 step = XM.MFP_symbols
 mfp_step = XM.samples_per_frame 
-print (mfp_step )
+
+# Loop for each possible MFP position
 max_corr=0
 max_index=0
 for loop in range(FSP_in_file-8*XM.FSP_per_frame):
-    corr_out=np.correlate( data_array[idx+mfp_step:idx+step+mfp_step],data_array[idx:idx+step],'full')
+
+    # Auto-correlate with next MFP
+    corr_out=np.correlate(data_array[idx+mfp_step:idx+step+mfp_step], data_array[idx:idx+step], 'full')
+
+    # Keep track of the maximum correlation
     if np.max(np.abs(corr_out))>max_corr:
         max_corr=np.max(np.abs(corr_out))
         max_index=idx+np.argmax(np.abs(corr_out))-XM.MFP_symbols+1
-        plt.clf()
-        plt.plot(np.abs(corr_out))
-        plt.xlabel('Index')
-        plt.ylabel('Magnitude')
-        plt.title('MFP Correlation')
-        plt.pause(0.1)
         print (max_index, max_corr)
-    idx=idx+samples_between_FSP
+        if debug:
+            plt.clf()
+            plt.plot(np.abs(corr_out))
+            plt.xlabel('Index')
+            plt.ylabel('Magnitude')
+            plt.title('MFP Correlation')
+            plt.pause(0.1)
+            
+    # Move to next possible MFP position
+    idx=idx+XM.samples_between_FSP
+
+# Show best case results
 plt.show()
+
+# Output MFP
 mfp_index=max_index
 print ("last index = ", idx)
 print ("mfp index = ", mfp_index)

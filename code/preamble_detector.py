dc49c3abd4e7d9064b76a0f83e5521f2e0455d83
osowatzke <osowatzke@gmail.com>
2025-05-07 05:12:13 +0000

    Aligning FSPs

--- code/preamble_detector.py
+++ code/preamble_detector.py
@@ -25,33 +25,66 @@ class preamble_detector(gr.basic_block):
         gr.basic_block.__init__(self,
             name="preamble_detector",
             in_sig=[np.complex64],
-            #out_sig=[np.float32])
-            out_sig=[(np.float32, self.FSP_SEP)])
+            out_sig=[(np.complex64, self.FSP_SEP),(np.float32, self.FSP_SEP),(np.float32, self.FSP_SEP)])
 
         fsp_taps = np.flip(np.conj(np.array(fsp)))
-        self.fsp_filt = filt(fsp_taps)
+        self.fsp_filt = filt(fsp_taps, dtype=np.complex64)
+        self.fsp_len = len(fsp_taps)
         
         self.sample_buffer = np.array([],dtype=np.complex64)
+        self.output_buffer = np.zeros(3*self.FSP_SEP, dtype=np.complex64)
+        self.corr_buffer   = np.zeros(3*self.FSP_SEP, dtype=np.float32)
+        
+        self.idx = -np.ones(3,dtype=np.int32)
+        self.last_idx = -1
 
     def general_work(self, input_items, output_items):
         
-        self.sample_buffer = np.concatenate((self.sample_buffer, input_items[0]))
+        buffer_size = min(len(output_items[0]), len(output_items[1]), len(output_items[2]))
+        samples_consumed = min(buffer_size*self.FSP_SEP - len(self.sample_buffer) - 1, len(input_items[0]))
+        self.sample_buffer = np.concatenate((self.sample_buffer, input_items[0][:samples_consumed]))
         produced = 0
         
         while len(self.sample_buffer) >= self.FSP_SEP:
+        
             x = self.sample_buffer[:self.FSP_SEP]
-            output_items[0][:] = np.abs(self.fsp_filt(x))
-            self.sample_buffer = self.sample_buffer[self.FSP_SEP:]
-            produced += 1
-
-        self.consume(0, len(input_items[0]))
+            self.sample_buffer = self.sample_buffer[self.FSP_SEP:]                
+            self.output_buffer = np.concatenate((self.output_buffer[self.FSP_SEP:], x))
+            
+            corr = np.abs(self.fsp_filt(x))
+            idx = np.argmax(corr)
+            avg_pwr = np.sum(np.abs(corr))/len(corr)
+            
+            if (corr[idx]/avg_pwr > 4):
+                idx -= self.fsp_len - 1
+                if idx < 0:
+                    idx += self.FSP_SEP
+            else:
+                idx = -1
+            
+            self.corr_buffer = np.concatenate((self.corr_buffer[self.FSP_SEP:], corr))
+            self.idx = np.concatenate((self.idx[1:], np.array([idx])))
+            
+            idx = self.idx[1]
+            if idx < 0:
+                idx = self.last_idx
+                
+            self.last_idx = idx
+            
+            if idx > 0:
+                output_items[0][produced][:] = self.output_buffer[idx:(idx + self.FSP_SEP)]              
+                output_items[1][produced][:] = self.corr_buffer[self.FSP_SEP:2*self.FSP_SEP]
+                output_items[2][produced][:] = self.corr_buffer[idx:(idx + self.FSP_SEP)]
+                
+                produced += 1
+            
+        self.consume(0, samples_consumed)
         return produced
 
-
 class filt:
-    def __init__(self, taps):
+    def __init__(self, taps, dtype=np.complex64):
         self.taps = np.array(taps)
-        self.filter_state = np.zeros(len(taps)-1, dtype=np.complex64)
+        self.filter_state = np.zeros(len(taps)-1, dtype=dtype)
         
     def __call__(self, x):
         x = np.array(x)
